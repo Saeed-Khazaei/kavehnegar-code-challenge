1b5e1cac5d051d5dfc73d53a3e4c76bea54aef7d
Saeed-khazaei <saeedkhazaei94@gmail.com>
2022-09-14 09:02:56 +0430

    add wrapper

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,10 +6,7 @@ const Layout = (props: { children: React.ReactNode }) => {
       <header className="h-16 w-full bg-white drop-shadow-xl sticky top-0" />
       <div className="w-full p-2 sm:p-5  md:p-10">
         <div className="w-full min-h-fit p-3 sm:p-6 md:p-12 bg-gray-100/100 rounded-2xl	">
-          <div className="w-full min-h-fit  ">
-            <div className="text-2xl font-bold mb-10">عنوان</div>
-            {props.children}
-          </div>
+          <div className="w-full min-h-fit ">{props.children}</div>
         </div>
       </div>
     </>
---
+++ components/Wrapper.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Wrapper = (props: { children: React.ReactNode; title: string }) => {
+  return (
+    <>
+      <div className="text-2xl font-bold mb-10">{props.title}</div>
+      {props.children}
+    </>
+  );
+};
+
+export default Wrapper;
