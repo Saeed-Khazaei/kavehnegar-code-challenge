293b75753043103963ccceb1bfcb39fe6a185e4f
Saeed-Khazaei <saeedkhazaei94@gmail.com>
2022-09-14 23:44:15 +0430

    change endpoint addressses

--- utils/endpoints.ts
+++ utils/endpoints.ts
@@ -1,6 +1,6 @@
 import { next_api } from "./constants";
 
 // Next Apis
-export const postTicketNextApi = `${next_api}/api/tickets/create`;
-export const getTicketsNextApi = `${next_api}/api/tickets/list`;
-export const getTicketNextApi = `${next_api}/api/tickets/ticket`;
+export const postTicketNextApi = `${next_api}/tickets/create`;
+export const getTicketsNextApi = `${next_api}/tickets/list`;
+export const getTicketNextApi = `${next_api}/tickets/ticket`;
