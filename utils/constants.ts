505f82c6aed462d374bd8cb70af1ce3ad4a32deb
Saeed-Khazaei <saeedkhazaei94@gmail.com>
2022-09-14 21:59:12 +0430

    add env and endpoints

---
+++ utils/constants.ts
@@ -0,0 +1,1 @@
+export const next_api = process.env.NEXT_API_URL;
---
+++ utils/endpoints.ts
@@ -0,0 +1,6 @@
+import { next_api } from "./constants";
+
+// Next Apis
+export const postTicketNextApi = `${next_api}/api/tickets/create`;
+export const getTicketsNextApi = `${next_api}/api/tickets/list`;
+export const getTicketNextApi = `${next_api}/api/tickets/ticket`;
