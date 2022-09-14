831e7d7495b877e0f63429fe028549e44c500930
Saeed-Khazaei <saeedkhazaei94@gmail.com>
2022-09-14 23:43:38 +0430

    add database and database helper

---
+++ helper/db.ts
@@ -0,0 +1,15 @@
+import path from 'path';
+import { promises as fs } from 'fs';
+import { TicketsResponse } from '../models/ticketsModel';
+
+const jsonDirectory = path.join(process.cwd(), 'helper');
+
+export const getDbDatas = async () => {
+  const oldData = await fs.readFile(jsonDirectory + '/data.json', 'utf8');
+  console.log('oldData', oldData)
+  return JSON.parse(oldData);
+}
+
+export const saveDbDatas = (data: TicketsResponse[]) => {
+  fs.writeFile(jsonDirectory + '/data.json', JSON.stringify(data))
+}
